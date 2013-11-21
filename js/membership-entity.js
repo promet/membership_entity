2e19bc64334656157041d3fa9d3d3882ee958f06
Caleb Thorne <caleb@calebthorne.com>
2013-11-21 15:14:13 -0700

    Select which fields are shown on the join forms

---
+++ js/membership-entity.js
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * Adds javascript for the membership_entity module.
+ */
+
+(function($) {
+
+  /**
+   * AJAX command to change an elment attribute.
+   */
+  Drupal.ajax.prototype.commands.membership_entity_attr = function(ajax, response, status) {
+    $(response.selector).attr(response.attribute, response.value);
+  }
+
+  /**
+   * Field instance settings screen: force the 'Display on join form.'
+   * checkbox checked whenever 'Required' is checked.
+   */
+  Drupal.behaviors.fieldMembershipEntityJoin = {
+    attach: function (context, settings) {
+      var $checkbox = $('form#field-ui-field-edit-form input#edit-instance-settings-membership-entity-join-form');
+      if ($checkbox.length) {
+        $('input#edit-instance-required', context).once('membership-entity-join-form-checkbox', function () {
+          $(this).bind('change', function (e) {
+            if ($(this).attr('checked')) {
+              $checkbox.attr('checked', true);
+            }
+          });
+        });
+      }
+    }
+  };
+
+})(jQuery);
+
+
