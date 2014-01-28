e3fd743fa4d69e599ee05b3f1b44342fb6d6b8bb
Caleb Thorne <caleb@calebthorne.com>
2014-01-28 10:24:12 -0700

    Fixes member_id and term length setting conflict

--- plugins/member_id/MembershipEntityNumericMemberId.class.php
+++ plugins/member_id/MembershipEntityNumericMemberId.class.php
@@ -11,7 +11,7 @@ class MembershipEntityNumericMemberId extends MembershipEntityMemberIdAbstract {
    */
   public function next() {
     $settings = $this->settings;
-    $length = $this->settings['length'];
+    $length = !empty($settings['length']) ? $settings['length'] : 5;
     $member_id = variable_get('membership_entity_next_member_id', 0);
     variable_set('membership_entity_next_member_id', ++$member_id);
     return str_pad($member_id, $length, '0', STR_PAD_LEFT);
@@ -27,6 +27,7 @@ class MembershipEntityNumericMemberId extends MembershipEntityMemberIdAbstract {
       '#title' => t('Member ID Length'),
       '#description' => t("The minimum number of digits for the member id. Numeric ids with fewer digits will be padded with '0's (eg. 00001)."),
       '#size' => 5,
+      '#required' => TRUE,
       '#default_value' => !empty($settings['length']) ? $settings['length'] : 5,
       '#element_validate' => array('element_validate_integer_positive'),
     );
