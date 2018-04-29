50263e9cdeb6d425c5711b21d95598f022cf98ec
Caleb Thorne <caleb@calebthorne.com>
2018-04-28 21:11:22 -0600

    Various spelling and code style updates

--- modules/membership_entity_term/js/membership-entity-term.js
+++ modules/membership_entity_term/js/membership-entity-term.js
@@ -14,6 +14,7 @@
       var $checkbox = $('form#field-ui-field-edit-form input#edit-instance-settings-membership-entity-term-renew-form');
       if ($checkbox.length) {
         $('input#edit-instance-required', context).once('membership-entity-term-renew-form-checkbox', function () {
+          // noinspection JSUnusedLocalSymbols
           $(this).bind('change', function (e) {
             if ($(this).attr('checked')) {
               $checkbox.attr('checked', true);
